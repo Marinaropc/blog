55d26c1a7db4f5071efa4393d63c3b175b6397de
Marina Romero <marinaropc@gmail.com>
2025-02-03 21:47:37 +0100

    fixed merge issues

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def like(post_id):
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5007, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
